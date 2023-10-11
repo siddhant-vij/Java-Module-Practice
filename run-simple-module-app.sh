bf4718fd64307d58b375b07da6445c457fed0c57
Siddhant Vij <siddhantvij201@gmail.com>
2023-10-11 16:49:53 +0530

    Java Module Practice

---
+++ run-simple-module-app.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java --module-path outDir -m main.app/com.moduletest.modules.main.MainApp
---
+++ simple-modules/main.app/com/moduletest/modules/main/MainApp.java
@@ -0,0 +1,9 @@
+package com.moduletest.modules.main;
+
+import com.moduletest.modules.hello.HelloModules;
+
+public class MainApp {
+    public static void main(String[] args) {
+        HelloModules.doSomething();
+    }
+}
